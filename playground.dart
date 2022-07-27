3465ec198c633b5e373bf2b8f88f3333a00e0c38
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-27 14:02:41 +0500

    raw string

--- playground.dart
+++ playground.dart
@@ -1,11 +1,9 @@
-
 import 'dart:io'; //importing the dart library
-
 
 /**
  * Mian method
  */
-void main(){
+void main() {
   /*
   var firstName = "Taimoor" ;
   String lastName = "Khan";
@@ -17,7 +15,7 @@ void main(){
   //Getting Input from User
   stdout.writeln("Enter your name: ");
   // you can use string instead of var
-  var name =  stdin.readLineSync();
+  var name = stdin.readLineSync();
   stdout.writeln("Hello $name");
   /**
    * Data types in Dart
@@ -27,7 +25,7 @@ void main(){
    * bool
    * dynamic
    */
-  int marks  = 100;
+  int marks = 100;
   print("Marks: $marks");
   double percentage = 90.5;
   print("Percentage: $percentage");
@@ -38,13 +36,30 @@ void main(){
 
   //dynamic is used to accept any type of data and its data type is not known at compile time
   // its data type is known at run time
-  
+
   dynamic data = "Taimoor";
 
   data = 100;
   print("Dynamic data: $data");
   // var is used to accept any type of data and its data type is not known at compile time
-  var lastName  = "Taimoor";
+  var lastName = "Taimoor";
   print("Last Name: $lastName");
-  
+
+//  Single qoute and double qoute are same unless in some situations below code represent that
+  var s1 = "Taimoor's Brothers";
+  var s2 = 'Taimoor\'s Brothers';
+
+  print("$s1");
+  print("$s2");
+
+  print(" ");
+
+  // raw string
+  var s3 = r"Taimoor's Brothers \n Get Special Things"; //output :Taimoor's Brothers \n Get Special Things
+  // if i dont put r  :
+  var s4 = "Taimoor's Brothers\n Get Special Things";
+
+  print("$s3");
+
+  print("$s4");
 }
