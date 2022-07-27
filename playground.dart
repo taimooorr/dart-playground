a340330518109678dc9d4650984462c10575dc52
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-27 13:54:23 +0500

    DataTypes Implemented

--- playground.dart
+++ playground.dart
@@ -19,5 +19,32 @@ void main(){
   // you can use string instead of var
   var name =  stdin.readLineSync();
   stdout.writeln("Hello $name");
+  /**
+   * Data types in Dart
+   * int
+   * double
+   * String
+   * bool
+   * dynamic
+   */
+  int marks  = 100;
+  print("Marks: $marks");
+  double percentage = 90.5;
+  print("Percentage: $percentage");
+  String name1 = "Taimoor";
+  print("Name: $name1");
+  bool isTrue = true;
+  print("answer: $isTrue");
 
+  //dynamic is used to accept any type of data and its data type is not known at compile time
+  // its data type is known at run time
+  
+  dynamic data = "Taimoor";
+
+  data = 100;
+  print("Dynamic data: $data");
+  // var is used to accept any type of data and its data type is not known at compile time
+  var lastName  = "Taimoor";
+  print("Last Name: $lastName");
+  
 }
