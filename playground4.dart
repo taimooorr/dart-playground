323fe80e95ae8d718cc4c06cf06987a8e13f144b
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-28 14:06:50 +0500

    inheritance

--- playground4.dart
+++ playground4.dart
@@ -3,46 +3,54 @@
 class Person {
 //Final keyword is used to make a variable immutable.
   
-//Static keyword is used to create a static variable.it can be call without creating an object. with class name.
+//Static keyword is used to create a static variable.it can be call without creating an object. with class type.
   static const int a = 10; // only static variable can be delared with constant.
 
   
-  late String name;
+  late String type;
   late int age;
   
 // Constructor 
-/*Person(String name, int age) {
-    this.name = name;
+Person(String type, int age) {
+    this.type = type;
     this.age = age;
-  }*/
+  }
   //Defualt Constructor
-  Person(String name, {int age = 18}) {
-    this.name = name;
+ /* Person(String type, {int age = 18}) {
+    this.type = type;
     this.age = 0;
   }
-  /* Short Method for CONstructor
-  Person(this.name, {this.age = 18});
   */
-  //Named Constructor
+  /* Short Method for CONstructor
+  Person(this.type, {this.age = 18});
+  */
+  //typed Constructor
 
-  Person.Taimoor(this.name, {this.age = 22});
+  Person.Taimoor(this.type, {this.age = 22});
 
   void display() {
-    print('Name: $name, Age: $age');
+    print('type: $type, Age: $age');
   }
 
 
 }
 
+//Inheritance
+class Taimoor extends Person {
+   String name = "gOOD PERSON";
+  Taimoor(this.name,String type, int age) : super(type, age);
+  void display() {
+    super.display();
+    print('Name: $name');
+  }
 
-
-
-
+}
 
 
 void main() {
-  var person = Person("Taimoor");
-  person.display();
+  
   var person2 = Person.Taimoor("Taimoor");
   person2.display();
+  var person = Taimoor('TAIMOOR', 'type', 12);
+  person.display();
 }
